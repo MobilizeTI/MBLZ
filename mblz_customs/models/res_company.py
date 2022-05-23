4417827f871b142077f9bebb47f9d5b039dec8a7
Felipe Angulo <felipe@mobilize.cl>
2022-05-23 16:20:09 -0400

    remove mblz_customs

--- mblz_customs/models/res_company.py
+++
@@ -1,8 +0,0 @@
-from odoo import fields, models
-from odoo.exceptions import UserError
-from odoo.tools.translate import _
-
-class ResCompany(models.Model):
-    _inherit = 'res.company'
-
-    mblz_api_url = fields.Char('Mobilize Api URL', readonly=False)
--- mblz_customs/models/res_config_settings.py
+++
@@ -1,7 +0,0 @@
-from odoo import fields, models
-
-class ResConfigSettings(models.TransientModel):
-
-    _inherit = 'res.config.settings'
-
-    mblz_api_url = fields.Char('Mobilize Api URL', related='company_id.mblz_api_url', readonly=False)
